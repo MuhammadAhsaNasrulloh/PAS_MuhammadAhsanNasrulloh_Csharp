c70691664adfde9cd53cd159bca118859ccd8e16
Muhammad Ahsan Nasrulloh <106051351+MuhammadAhsaNasrulloh@users.noreply.github.com>
2023-12-03 07:54:18 +0700

    Add files via upload

---
+++ PAS_MuhammadAhsanNasrulloh/Form3.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace PAS_MuhammadAhsanNasrulloh
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtNISN = new System.Windows.Forms.TextBox();
+            this.cbKelas = new System.Windows.Forms.ComboBox();
+            this.cbJurusan = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(647, 236);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(469, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CRUD DATABASE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NISN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(490, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kelas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(490, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 22);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Jurusan";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(991, 270);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(89, 43);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(186, 183);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(184, 27);
+            this.txtNama.TabIndex = 9;
+            // 
+            // txtNISN
+            // 
+            this.txtNISN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNISN.Location = new System.Drawing.Point(186, 87);
+            this.txtNISN.Name = "txtNISN";
+            this.txtNISN.Size = new System.Drawing.Size(184, 27);
+            this.txtNISN.TabIndex = 10;
+            // 
+            // cbKelas
+            // 
+            this.cbKelas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbKelas.FormattingEnabled = true;
+            this.cbKelas.Items.AddRange(new object[] {
+            "X",
+            "XI",
+            "XII"});
+            this.cbKelas.Location = new System.Drawing.Point(573, 89);
+            this.cbKelas.Name = "cbKelas";
+            this.cbKelas.Size = new System.Drawing.Size(136, 28);
+            this.cbKelas.TabIndex = 11;
+            // 
+            // cbJurusan
+            // 
+            this.cbJurusan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbJurusan.FormattingEnabled = true;
+            this.cbJurusan.Items.AddRange(new object[] {
+            "AKL",
+            "BD",
+            "BR",
+            "DKV",
+            "KL",
+            "RPL",
+            "TL",
+            "TKJ",
+            "TPM",
+            "TO"});
+            this.cbJurusan.Location = new System.Drawing.Point(573, 182);
+            this.cbJurusan.Name = "cbJurusan";
+            this.cbJurusan.Size = new System.Drawing.Size(136, 28);
+            this.cbJurusan.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(773, 270);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 43);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(880, 270);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(89, 43);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(773, 351);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(89, 43);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 544);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbJurusan);
+            this.Controls.Add(this.cbKelas);
+            this.Controls.Add(this.txtNISN);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtNISN;
+        private System.Windows.Forms.ComboBox cbKelas;
+        private System.Windows.Forms.ComboBox cbJurusan;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ PAS_MuhammadAhsanNasrulloh/Form3.cs
@@ -0,0 +1,134 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAS_MuhammadAhsanNasrulloh
+{
+    public partial class Form3 : Form
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=pas_muhammadahsan");
+        void command(String query)
+        {
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        void showData()
+        {
+            try
+            {
+                conn.Open();
+                MySqlDataAdapter sda = new MySqlDataAdapter("select * from tb_data", conn);
+                DataTable dt = new DataTable();
+
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        void clear()
+        {
+            txtNISN.Text = string.Empty;
+            txtNama.Text = string.Empty;
+            cbKelas.Text = string.Empty;
+            cbJurusan.Text = string.Empty;
+            showData();
+        }
+        public Form3()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtNISN.Text == string.Empty || txtNama.Text == string.Empty || cbKelas.Text == string.Empty || cbJurusan.Text == string.Empty)
+            {
+                MessageBox.Show("semua kolom harus di isi!");
+            }
+            else
+            {
+                command("insert into tb_data(NIS, Nama, Kelas, Jurusan) values ('" + txtNISN.Text + "', '" + txtNama.Text + "', '" + cbKelas.Text + "', '" + cbJurusan.Text + "')");
+                clear();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtNISN.Text == string.Empty || txtNama.Text == string.Empty || cbKelas.Text == string.Empty || cbJurusan.Text == string.Empty)
+            {
+                MessageBox.Show("semua kolom harus di isi!");
+            }
+            else
+            {
+                command("update tb_data set NIS  = '" + txtNISN.Text + "', Nama = '" + txtNama.Text + "', Kelas = '" + cbKelas.Text + "', Jurusan = '" + cbJurusan.Text + "' where NIS = '" + txtNISN.Text + "'");
+                clear();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtNISN.Text == string.Empty || txtNama.Text == string.Empty || cbKelas.Text == string.Empty || cbJurusan.Text == string.Empty)
+            {
+                MessageBox.Show("semua kolom harus di isi!");
+            }
+            else
+            {
+                command("delete from tb_data where NIS = '" + txtNISN.Text + "'");
+                clear();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private int selectedRowIndex;
+        DataTable table = new DataTable();
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow dr = this.dataGridView1.Rows[e.RowIndex];
+
+            txtNISN.Text = dr.Cells[0].Value.ToString();
+            txtNama.Text = dr.Cells[1].Value.ToString();
+            cbKelas.Text = dr.Cells[2].Value.ToString();
+            cbJurusan.Text = dr.Cells[3].Value.ToString();
+
+            selectedRowIndex = e.RowIndex;
+        }
+    }
+}
+    
